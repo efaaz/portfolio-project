80637378fbe4525a67f6161ccab06f73016618f3
Wasifur Rahman Efaz <110248681+efaaz@users.noreply.github.com>
2024-10-19 23:19:33 +0600

    added header block

--- src/app/page.js
+++ src/app/page.js
@@ -1,101 +1,98 @@
-import Image from "next/image";
+"use client";
+import { FiArrowRight, FiMapPin, FiMail } from "react-icons/fi";
+import { Typewriter } from "react-simple-typewriter";
+import { IoLogoJavascript } from "react-icons/io";
+import "animate.css";
+import { useMyContext } from "@/components/Providers";
+import { twMerge } from "tailwind-merge";
+import { motion } from "framer-motion";
 
 export default function Home() {
+  const { isAboutFirst, isRobotFirst, isContactFirst } = useMyContext();
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.js
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+    <div className="min-h-screen px-4 py-8">
+      <motion.div
+        initial="initial"
+        animate="animate"
+        transition={{
+          staggerChildren: 0.05,
+        }}
+        className="mx-auto grid max-w-4xl grid-flow-dense grid-cols-12 gap-4"
+      >
+        <HeaderBlock />
+      </motion.div>
     </div>
   );
 }
+const Block = ({ className, ...rest }) => {
+  return (
+    <motion.div
+      variants={{
+        initial: {
+          scale: 0.5,
+          y: 50,
+          opacity: 0,
+        },
+        animate: {
+          scale: 1,
+          y: 0,
+          opacity: 1,
+        },
+      }}
+      transition={{
+        type: "spring",
+        mass: 3,
+        stiffness: 400,
+        damping: 50,
+      }}
+      className={twMerge(
+        "col-span-4 rounded-lg border border-zinc-700 bg-secondary-400 p-6",
+        className
+      )}
+      {...rest}
+    />
+  );
+};
+
+const HeaderBlock = () => (
+  <Block
+    whileHover={{
+      scale: 1.03,
+    }}
+    className="col-span-12 row-span-2 md:col-span-6"
+  >
+    <Image
+      src="https://i.ibb.co.com/WxPv3pF/AGC-20240912-123537361-PORTRAIT-1.jpg"
+      alt="avatar"
+      className="mb-4 border-4 border-maheenWT1 size-20 object-cover rounded-full"
+      width={1200} // Specify width attribute
+      height={1200} // Specify height attribute
+    />
+    <h1 className="mb-12 text-4xl font-medium leading-tight">
+      Hi, I&#39;m{" "}
+      <span>
+        {/* Style will be inherited from the parent element */}
+        <Typewriter
+          words={["Maheen"]}
+          loop={0}
+          cursor
+          cursorStyle="_"
+          typeSpeed={50}
+          deleteSpeed={40}
+          delaySpeed={3000}
+        />
+      </span>
+      <p className="text-zinc-500 text-xl">
+        a Web Developer from Bangladesh with experience in responsive and user
+        friendly Website development.
+      </p>
+    </h1>
+    <a
+      href="https://drive.google.com/uc?export=download&id=1henj73qXOi7c9IlH8_881z9SxgSvTo1w"
+      download
+      className="flex items-center  gap-1 animate__animated animate__fadeIn  animate__slower animate__infinite text-red-300 hover:underline"
+    >
+      Download Resume <FiArrowRight />
+    </a>
+  </Block>
+);
