07e2ee270aae145c948cd3fe4ab1b47605f23642
Wasifur Rahman Efaz <110248681+efaaz@users.noreply.github.com>
2024-10-19 21:06:16 +0600

    install some packages and tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,17 +4,20 @@ module.exports = {
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
- 
-    // Or if using `src` directory:
     "./src/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
+      backgroundImage: {
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        efazWT1: "rgb(191 191 177  / var(--tw-text-opacity))",
+        efazBB1: "rgb(20 20 10 / var(--tw-bg-opacity))",
       },
     },
   },
-  plugins: [],
+  plugins: [require("daisyui")],
 };
